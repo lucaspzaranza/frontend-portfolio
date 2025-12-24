1a42698eb7c80150245d918b89f36b3a97e4dc57
Lucas Zaranza <lucaszaranza@gmail.com>
2025-12-24 10:39:59 -0300

    fixed FOUC (Flash of Untransaleted Content) buh when in eng-US

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -8,23 +8,31 @@ type LanguageContextType = {
   language: Language
   toggleLanguage: () => void
   t: typeof translations.pt
+  ready: boolean
 }
 
 const LanguageContext = createContext<LanguageContextType | null>(null)
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
   const [language, setLanguage] = useState<Language>('pt')
+  const [ready, setReady] = useState(false)
 
   useEffect(() => {
-    const browserLanguage = navigator.language.toLowerCase()
+    const browserLang = navigator.language.toLowerCase()
 
-    if (!browserLanguage.startsWith('pt')) {
+    if (!browserLang.startsWith('pt')) {
       setLanguage('en')
     }
+
+    setReady(true)
   }, [])
 
   const toggleLanguage = () => {
     setLanguage(prev => (prev === 'pt' ? 'en' : 'pt'))
+  }
+
+  if (!ready) {
+    return null
   }
 
   return (
@@ -32,7 +40,8 @@ export function LanguageProvider({ children }: { children: React.ReactNode }) {
       value={{
         language,
         toggleLanguage,
-        t: translations[language]
+        t: translations[language],
+        ready
       }}
     >
       {children}
@@ -43,7 +52,7 @@ export function LanguageProvider({ children }: { children: React.ReactNode }) {
 export const useLanguage = () => {
   const context = useContext(LanguageContext)
   if (!context) {
-    throw new Error('useLanguage must be used within a LanguageProvider')
+    throw new Error('useLanguage must be used within LanguageProvider')
   }
   return context
 }
